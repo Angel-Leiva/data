8ffcf73e0ac36f9df019fcbe332ad1bfe1ff8718
Angel-Leiva <146677476+Angel-Leiva@users.noreply.github.com>
2026-06-11 09:09:24 -0600

    Rename import from init_state to init

--- Frontend/streamlit_app.py
+++ Frontend/streamlit_app.py
@@ -7,7 +7,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[3]
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
-from Frontend import init_state
+from Frontend import init
 from Backend.config.settings import APP_TITLE
 
 
